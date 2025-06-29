53fbf147f2cf7e459653551f85b758e1fd0f3dd2
Evoless <evoless@Evoless.localdomain>
2025-06-30 00:04:08 +0100

    Let's Build Your Database: Your Gateway to Data Adventure

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
